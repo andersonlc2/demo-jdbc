0496be3b945839335d93539edb93fc085358c2f0
andersonlc2 <acorreia610@gmail.com>
2022-03-01 13:51:24 -0300

    update and delete seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: seller findById ===");
@@ -32,5 +34,18 @@ public class Program {
 		sellerDao.insert(obj);
 		System.out.println("Inserted! New id = " + obj.getId());
 		
+		System.out.println("\n=== TEST 5: seller update ===");
+		obj = sellerDao.findById(14);
+		obj.setName("Martha Waine");
+		sellerDao.update(obj);
+		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.println("Enter id for delete teste: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 }
