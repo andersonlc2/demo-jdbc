37778809e5aa95181edf365673f29f387053392c
andersonlc2 <acorreia610@gmail.com>
2022-03-01 11:23:53 -0300

    create Department and seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Date;
 
-import db.DB;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Department d = new Department(1, "Books");
+		Seller s = new Seller(21, "Bob", "bob@gmail.com", new Date(), 2000.0, d);
 		
+		System.out.println(s);
 	}
-
 }
